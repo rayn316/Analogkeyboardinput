fc23c1e70348bb4592d22c3b7af40fd3513744b1
赵尚 <244067545@qq.com>
2019-02-11 20:21:30 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,92 @@
+#coding:utf-8
+
+import win32clipboard as w
+import win32con
+import win32api
+import time
+
+Keyboardcodes = '''aA 65
+bB 66
+cC 67
+dD 68
+eE 69
+fF 70
+Gg 71
+hH 72
+iI 73
+jJ 74
+kK 75
+lL 76
+mM 77
+nN 78
+oO 79
+pP 80
+qQ 81
+rR 82
+sS 83
+tT 84
+uU 85
+vV 86
+wW 87
+xX 88
+yY 89
+zZ 90
+0) 48
+1! 49
+2@ 50
+3# 51
+4$ 52
+5% 53
+6^ 54
+7& 55
+8* 56
+9( 57
+;: 186
+=+ 187
+,< 188
+-_ 189
+.> 190
+/? 191
+`~ 192
+[{ 219
+\| 220
+]} 221
+'" 222'''
+Keyboardcode = {}
+Keyboardcode[' '] = [1, 32]
+for i in Keyboardcodes.split('\n'):
+    key, vaule = i.split(' ')
+    vaule = int(vaule)
+    Keyboardcode[key[0]] = [1, vaule]
+    Keyboardcode[key[1]] = [2, vaule]
+
+# 获取剪切板内容
+def gettext():
+    w.OpenClipboard()
+    t = w.GetClipboardData(win32con.CF_TEXT)
+    w.CloseClipboard()
+    return t
+
+def start(text):
+#第一个参数，键盘对应数字，查表
+#第二个，第四个没用
+#第三个参数，0代表按下，win32con.KEYEVENTF_KEYUP松开
+    for line in text.split('\r\n'):
+        for i in line:
+            j = Keyboardcode[i]
+            if j[0] == 1:
+                win32api.keybd_event(j[1], 0, 0, 0)
+                win32api.keybd_event(j[1], 0, win32con.KEYEVENTF_KEYUP, 0)
+            elif j[0] == 2:
+                win32api.keybd_event(16, 0, 0, 0)
+                win32api.keybd_event(j[1], 0, 0, 0)
+                win32api.keybd_event(j[1], 0, win32con.KEYEVENTF_KEYUP, 0)
+                win32api.keybd_event(16, 0,     win32con.KEYEVENTF_KEYUP, 0)
+        win32api.keybd_event(13, 0, 0, 0)
+        win32api.keybd_event(13, 0, win32con.KEYEVENTF_KEYUP, 0)
+        # 不限制速度会导致粘贴中断（虚拟机）
+        time.sleep(0.3)
+
+#给鼠标移动时间
+time.sleep(3)
+start(gettext())
